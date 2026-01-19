9d7e2b76cf1f20bf897ef1b22bc3360902c494ac
MacOS <macos@MacBook-Pro-cua-MacOS-3.local>
2026-01-19 16:30:08 +0700

    push code

--- main.js
+++ main.js
@@ -11,12 +11,12 @@ function Product(id, name, price, quantity, category, isAvailable) {
 }
 // câu 2
 const products = [
-    Product(1, "SP 1", 100000, 10, "Áo", true),
-    Product(2, "SP 2", 200000, 5, "Quần", true),
-    Product(3, "SP 3", 300000, 0, "Giày", false),
-    Product(4, "SP 4", 400000, 8, "Mũ", true),
-    Product(5, "SP 5", 500000, 2, "Túi", true),
-]
+    Product(1, "T-Shirt Basic", 100000, 10, "clothing", true),
+    Product(2, "Slim Fit Jeans", 200000, 5, "pants", true),
+    Product(3, "Running Sneakers", 300000, 0, "shoes", false),
+    Product(4, "Baseball Cap", 400000, 8, "hats", true),
+    Product(5, "Leather Belt", 500000, 2, "accessories", true),
+];
 console.log("products => ", products);
 
 // câu 3
@@ -35,7 +35,7 @@ const hasProductAbove100000 = products.some(product => product.price > 100000);
 console.log("hasProductAbove100000 => ", hasProductAbove100000);
 
 // câu 6
-const allShoesAreAvailable = products.every(product => product.category === "Giày" && product.isAvailable);
+const allShoesAreAvailable = products.some(product => product.category === "accessories" && product.isAvailable);
 console.log("allShoesAreAvailable => ", allShoesAreAvailable);
 
 // câu 7 
@@ -48,11 +48,15 @@ for (const product of products) {
 }
 
 // câu 9
-for (const key in products) {
-    console.log("key => ", key, " - ", products[key]);
+for (const i in products) {
+    console.log(`--- Product ${Number(i) + 1} ---`);
+
+    for (const key in products[i]) {
+        console.log(`${key}:`, products[i][key]);
+    }
 }
 
-// câu 10 lấy danh sách đang bán và còn hàng
+// câu 10 
 const availableAndInStockProducts = products.filter(product => product.isAvailable && product.quantity > 0);
 console.log("availableAndInStockProducts => ", availableAndInStockProducts);
 
